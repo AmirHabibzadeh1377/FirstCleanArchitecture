f18d3c28ffe92cc13ba53aca722d79e0e359baca
amirhabibzadehHome <amirhabibzadeh1350@gmail.com>
2023-10-07 12:31:18 +0330

    added email service and impelement

---
+++ CleanArchitecture.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection ConfigureInfrastractureServiceRegistration(IServiceCollection services , IConfiguration configuration)
+        {
+
+        }
+    }
+}
---
+++ CleanArchitecture.Infrastructure/Mail/EmailSender.cs
@@ -0,0 +1,30 @@
+﻿using CleanArichitecture.Application.Infrastracture;
+using CleanArichitecture.Application.Models;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Mail
+{
+    public class EmailSender : IEmailSender
+    {
+        #region Fields
+
+        private readonly IOptions<EmailSettingModel> _emailSetting;
+
+        #endregion
+
+        #region Ctor
+
+        public EmailSender(IOptions<EmailSettingModel> emailSetting)
+        {
+            _emailSetting = emailSetting;
+        }
+
+        #endregion
+
+        public Task<bool> SendEmail(EmailModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
