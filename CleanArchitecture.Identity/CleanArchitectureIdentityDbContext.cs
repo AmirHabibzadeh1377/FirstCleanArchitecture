811b52cf286fe3a6da9d4ef9a759688bea7d3296
amirhabibzadeh <amirhabibzadeh1350@gmail.com>
2023-10-30 17:37:55 +0330

    changes

---
+++ CleanArchitecture.Identity/CleanArchitectureIdentityDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchitecture.Identity.Models;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Identity
+{
+    public class CleanArchitectureIdentityDbContext:IdentityDbContext<ApplicationUser>
+    {
+        #region Ctor
+
+        public CleanArchitectureIdentityDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        #endregion
+    }
+}
